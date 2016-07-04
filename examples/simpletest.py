4dd0c4a695b5bde044b9e1ef398e3d1a65288c57
mB-PiBox <marshallbaer@gmail.com>
2016-07-04 05:35:39 -0500

    print temp in fahrenheit
    added line to print temp in fahrenheit below celsius.

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -46,6 +46,7 @@ sensor = BMP085.BMP085()
 #sensor = BMP085.BMP085(mode=BMP085.BMP085_ULTRAHIGHRES)
 
 print('Temp = {0:0.2f} *C'.format(sensor.read_temperature()))
+print('Temp = {0:0.2f} *F'.format(sensor.read_temperature_f()))
 print('Pressure = {0:0.2f} Pa'.format(sensor.read_pressure()))
 print('Altitude = {0:0.2f} m'.format(sensor.read_altitude()))
 print('Sealevel Pressure = {0:0.2f} Pa'.format(sensor.read_sealevel_pressure()))
